1fd2cc4ba53a9156af87996388829b2fc7d818e3
Nicolae Grigoriu <ng@inlead.dk>
2017-09-05 15:48:55 +0300

    EAYSOPAC-590 - Temoporary commit for custom filters.

---
+++ templates/panels/kultur-events-filters.tpl.php
@@ -0,0 +1,8 @@
+<h3><?php print t('Library'); ?></h3>
+<div id="filter_libraries">
+  <?php print $libraries; ?>
+</div>
+<h3><?php print t('Category'); ?></h3>
+<div id="filter_categories">
+  <?php print $categories; ?>
+</div>
