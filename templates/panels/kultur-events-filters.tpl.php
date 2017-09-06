a901db965d73a1f96bafcd99d6c26444b4033622
Nicolae Grigoriu <ng@inlead.dk>
2017-09-06 14:08:03 +0300

    EASYOPAC-590 - Remove unnecessary template file.

--- templates/panels/kultur-events-filters.tpl.php
+++
@@ -1,8 +0,0 @@
-<h3><?php print t('Library'); ?></h3>
-<div id="filter_libraries">
-  <?php print $libraries; ?>
-</div>
-<h3><?php print t('Category'); ?></h3>
-<div id="filter_categories">
-  <?php print $categories; ?>
-</div>
